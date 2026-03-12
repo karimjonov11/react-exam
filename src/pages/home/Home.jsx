0937627ee5e1d4d134754547dd02a60f0ec937e6
Your Name <karimjonovabrorxontg@gmail.com>
2026-03-12 05:08:45 +0500

    Fix: Sec2 case sensitivity import

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import './Home.css'
 import Sec1 from './Sec1'
-import Sec2 from './sec2'
+import Sec2 from './Sec2'
 import Sec3 from './Sec3'
 import Sec4 from './Sec4'
 import Sec5 from './Sec5'
@@ -10,13 +10,13 @@ import Sec7 from './Sec7'
 const Home = () => {
   return (
     <>
-    <Sec1 />
-    <Sec2 />
-    <Sec3 />
-    <Sec4 />
-    <Sec5 />
-    <Sec6 />
-    <Sec7 />
+      <Sec1 />
+      <Sec2 />
+      <Sec3 />
+      <Sec4 />
+      <Sec5 />
+      <Sec6 />
+      <Sec7 />
     </>
   )
 }
